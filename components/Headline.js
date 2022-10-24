a35d711aacda894e29c9bb76dcc69545a11090d4
takumi <takura010719@gmail.com>
2022-10-24 16:02:18 +0900

    ♻️ export defaultの削除

--- components/Headline.js
+++ components/Headline.js
@@ -3,7 +3,7 @@ import { Footer } from "../components/Footer";
 import React from "react";
 import styles from "../styles/Home.module.css";
 
-export default function Headline(props) {
+export function Headline(props) {
   return (
     <div>
       <h1 className={styles.title}>{props.page} page</h1>
